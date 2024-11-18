81e04bba5207c436bf92a3edee4f507b26183346
Anouar Dahdah <102748716+AnouarDahdah@users.noreply.github.com>
2024-11-18 20:18:35 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+```python
+from setuptools import setup, find_packages
+
+setup(
+    name='sdf-autoencoder',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'numpy',
+        'plotly',
+        'scikit-image',
+        'pyyaml',
+    ],
+    author='Your Name',
+    description='SDF Autoencoder for Sphere Reconstruction',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    python_requires='>=3.8',
+)
+```
